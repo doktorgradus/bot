3192bec1fad4ec83d6fc95a903a7a76a5706add3
systemfailure <kefzce@gmail.com>
2016-11-04 04:32:25 +0200

    upd4

--- core/commands/gif/gif.php
+++ core/commands/gif/gif.php
@@ -1,5 +1,20 @@
 <?php
     $photo_id = array();
+    $photo_id[] = "AAQCABNP9IENAAQ8IlXQsbxbxP-0AAIC";
+    $photo_id[] = "AAQCABMPWIQqAATbajVy5S7CH4rGAQABAg";
+    $photo_id[] = "AAQCABOGIoMqAAR-_CQz-44bDbYMAAIC";
+    $photo_id[] = "BQADAgADEwADf0p-EK4baAro0kg6Ag";
+    $photo_id[] = "AAQCABNSmIQqAAQwqHwJaIF2BTYQAAIC";
+    $photo_id[] = "AAQCABMH3IUqAAQyal0Ln5nq3qY3AAIC";
+    $photo_id[] = "AAQCABPkyIENAAQ7t-J8-nTT_IKxAAIC";
+    $photo_id[] = "AAQCABMTB4INAASkniu_vjbLdsmzAAIC";
+    $photo_id[] = "AAQCABNPlIQqAAR2msB28DrJR9QOAAIC";
+    $photo_id[] = "BQADAgADGQADf0p-EMN4DeV_p9VRAg";
+    $photo_id[] = "BQADAgADGwADf0p-EC4SbRhjYqwbAg";
+    $photo_id[] = "BQADAgADHAADf0p-EDzOoI4PmScRAg";
+    $photo_id[] = "BQADAgADHQADf0p-EEguK_QgvpkkAg";
+    $photo_id[] = "BQADAgADHgADf0p-EGgMWUFbwMylAg";
+    $photo_id[] = "BQADAgADHwADf0p-EKtrAZ-qY4JIAg";
     $photo_id[] = "";
     $rand = array_rand($photo_id);
     $count = count($photo_id);
