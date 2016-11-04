0591a48c9a2fe09a3ec4db1c4f95acbb65408ff6
kefzce <kefzce@gmail.com>
2016-11-04 02:51:04 +0200

    Update answers.php

--- core/answers.php
+++ core/answers.php
@@ -9,7 +9,16 @@ switch($message) {
     $keyboard=array("inline_keyboard"=>$inline_keyboard);
     $replyMarkup = json_encode($keyboard);
     sendPhoto($chat_id,$photo_id[$rand],$msgid,"Тебе достался вариант №: ".$rand." из ".$count,$replyMarkup);
-    break;       
+    break;  
+           case 'tits':  
+     include 'core/commands/tits/tits.php';
+    $inline_button1 = array("text"=>"👍","callback_data"=>'/plz');
+    $inline_button2 = array("text"=>"👎","callback_data"=>'/votedown');
+    $inline_keyboard = [[$inline_button1,$inline_button2]];
+    $keyboard=array("inline_keyboard"=>$inline_keyboard);
+    $replyMarkup = json_encode($keyboard);
+    sendPhoto($chat_id,$photo_id[$rand],$msgid,"Тебе достался вариант №: ".$rand." из ".$count,$replyMarkup);
+    break;
     
 //     //START OF THE GAME
 //         case '[1]':
