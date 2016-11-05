15effd665a1c3fe403490d8796b44ccc00260a3f
systemfailure <kefzce@gmail.com>
2016-11-05 14:13:45 +0200

    updated setting

--- core/settings.php
+++ core/settings.php
@@ -21,7 +21,7 @@ $user_id_group = $output['message']['from']['id'];
 $user_first_name_group = $output['message']['from']['first_name'];
 $user_name_group = $output['message']['from']['username'];
 // VIP USERS
-$vip_users = ["276712063","233780968","76484596","4"];
+$vip_users = ["276712063","233780968","76484596","298347855","92116599"];
 // END VIP USERS
 $emoji = array(
   'preload' => json_decode('"\uD83D\uDE03"'), // Улыбочка.
