4c0c8f0ecae640218d16604dfa167b72893256e7
systemfailure <kefzce@gmail.com>
2016-11-05 15:23:22 +0200

    Vip

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 
 
 
-// if ($user_name_group == 'Denormalization') {
+if(!in_array($user_id_group, $vip_users)){
 	 if ($sticker) {
 	sendMessage($chat_id,"ID сообщения: ".$msgid. " Имя пользователя : ".$user_first_name_group.$first_name." ID пользователя: ".$user_id_group." Никнейм: ".$username.$user_name_group." Время: ".date('H:i:s',time())." Я вижу ты отправил стикер... <b>не надо</b> так ",$msgid);
 }
-
+}
 echo "<hr><b> All files loaded correctly, SemsGood !!! </b></hr><br>";
 echo '<img src="https://static-cdn.jtvnw.net/emoticons/v1/64138/3.0">';
 echo '<a href="/web">Web form</a>'
