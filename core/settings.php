08346f85778d94834ff016597f2519af5af497be
systemfailure <kefzce@gmail.com>
2016-11-06 00:06:10 +0200

    add new vip user

--- core/settings.php
+++ core/settings.php
@@ -21,7 +21,7 @@ $user_id_group = $output['message']['from']['id'];
 $user_first_name_group = $output['message']['from']['first_name'];
 $user_name_group = $output['message']['from']['username'];
 // VIP USERS
-$vip_users = ["276712063","233780968","76484596","298347855","92116599"];
+$vip_users = ["276712063","233780968","76484596","298347855","92116599","123123"];
 $count_vips = count($vip_users);
 // END VIP USERS
 $emoji = array(
