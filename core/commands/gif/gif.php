31ef66c19014143255dab91d583bbe81d066d711
systemfailure <kefzce@gmail.com>
2016-11-05 06:48:33 +0200

    add gifs

--- core/commands/gif/gif.php
+++ core/commands/gif/gif.php
@@ -7,6 +7,17 @@
     "BQADAgADHQADf0p-EEguK_QgvpkkAg",
     "BQADAgADHgADf0p-EGgMWUFbwMylAg",
     "BQADAgADHwADf0p-EKtrAZ-qY4JIAg",
+    "BQADAgADLQADf0p-ELwPkuQN6AfeAg",
+    "BQADAgADLgADf0p-EPz-RV1Cr06fAg",
+    "BQADAgADLwADf0p-EE2cLUl63iySAg",
+    "BQADAgADMAADf0p-EIijzqDPQcrrAg",
+    "BQADAgADMQADf0p-EF3xzwjPz4yuAg",
+    "BQADAgADMgADf0p-EBnIcs_cxCEFAg",
+    "BQADAgADMwADf0p-EL0uU6mz9welAg",
+    "BQADAgADNAADf0p-EIt9DXGksBYMAg",
+    "BQADAgADNQADf0p-EB--PJJrQAk9Ag",
+    "BQADAgADNgADf0p-EEWtMZy7OHD7Ag",
+    "BQADAgADNwADf0p-ENlIWWc7CbBvAg",
     ];
 
     $rand = array_rand($document_id);
