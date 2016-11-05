83b22ed193ba2701f2f0b25879d1f773ae6bc408
systemfailure <kefzce@gmail.com>
2016-11-05 06:34:04 +0200

    HTML

--- core/answers.php
+++ core/answers.php
@@ -8,7 +8,7 @@ switch($message) {
      include 'core/commands/tits/tits.php';
         break;
     case '/id':
-        sendMessage($chat_id,"Твой ID: {$user_id_group}",$msgid);
+        sendMessage($chat_id,"Твой ID: <b>{$user_id_group}</b>",$msgid);
     break;
         case '/gif':
     if ($user_id_group == '276712063' or '233780968'){
@@ -40,7 +40,7 @@ switch($message) {
             include 'commands/bash.php';
     break;
     case '/id@phphelperbot':
-        sendMessage($chat_id,"Твой ID: {$user_id_group}",$msgid);
+        sendMessage($chat_id,"Твой ID: <b>{$user_id_group}</b>",$msgid);
     break;
         case '/calendar@phphelperbot':
             include 'commands/calendar.php';
