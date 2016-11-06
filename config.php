c1c5c0a5a42b54145ef0c35c7983ff971a604388
systemfailure <kefzce@gmail.com>
2016-11-07 00:14:55 +0200

    asd

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-  'token' => '123qwe',
+  'token' => '123qwe123',
   'commands' => 'getMe',
 ];
--- web/index.php
+++ web/index.php
@@ -1,4 +1,6 @@
 <?php
-phpinfo(); 
+echo json_decode(file_get_contents("php://input"),true);
+echo (file_get_contents("php://input"));
+print_r(file_get_contents("php://input"));
 echo '<a href="images.php">GET_FILE_ID</a>';
 ?>
