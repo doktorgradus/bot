d56e3da1cda35d142ab199d608ddd890f782a38a
systemfailure <kefzce@gmail.com>
2016-11-06 04:02:57 +0200

    asd

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require "../vendor/autoload.php";
+require "/vendor/autoload.php";
 /////////////////////////////////////
  require 'core/functions.php';     //
  require 'core/settings.php';      //
