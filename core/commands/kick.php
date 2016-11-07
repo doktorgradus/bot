b383c62f94599439230e25bc9423a7bf3592180d
systemfailure <kefzce@gmail.com>
2016-11-07 07:38:56 +0200

    rofle

--- core/commands/kick.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($message) {
-	sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
-	$message = explode(",", $message);
-	sendMessage($chat_id,"Кикаем юзера {$message[1]}",$msgid);
-	
-}
--- index.php
+++ index.php
@@ -11,11 +11,13 @@ require __DIR__ . '/vendor/autoload.php';
 
         if(in_array($user_id_group, $vip_users)){
         	if (preg_match_all("/(?<![\w\d])(кик,[0-9])(?![\w\d])/uim",$message_preg, $mathes)) {
-    include 'commands/kick.php';
+    	sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
+	$message = explode(",", $message);
+	sendMessage($chat_id,"Кикаем юзера {$message[1]}",$msgid);
         	}
 }
 //catch non vip users who spam stickers.
-if(!in_array($user_id_group, $vip_users)){
+if(in_array($user_id_group, $vip_users)){
 	 if ($sticker) {
 	sendMessage($chat_id,"ID сообщения: ".$msgid. " Имя пользователя : ".$user_first_name_group.$first_name." ID пользователя: ".$user_id_group." Никнейм: ".$username.$user_name_group." Время: ".date('H:i:s',time())." Я вижу ты отправил стикер... <b>не надо</b> так ",$msgid);
 	//kickChatMember($chat_id,$user_id);
