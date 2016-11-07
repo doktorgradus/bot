09d6d8a6776880ec2e5d2ed77987fed4327f41c5
systemfailure <kefzce@gmail.com>
2016-11-08 01:37:47 +0200

    asd

--- index.php
+++ index.php
@@ -8,6 +8,21 @@ require __DIR__ . '/vendor/autoload.php';
 /////////////////////////////////////
 
 
+        if(in_array($user_id_group, $vip_users)){
+        	if (preg_match_all("/(?<![\w\d])(gif [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
+    	//sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
+	$message = explode(" ", $message);
+	include 'core/commands/gif/gif.php';
+    $inline_button1 = array("text"=>"👍","callback_data" =>'/voteup');
+    $inline_button2 = array("text"=>"👎","callback_data" =>'/votedown');
+    $inline_keyboard = [[$inline_button1,$inline_button2]];
+    $keyboard=array("inline_keyboard"=>$inline_keyboard);
+    $replyMarkup = json_encode($keyboard);
+    sendChatAction($chat_id, "upload_photo");
+    sendPhoto($chat_id,$document_id[$message[1]],$msgid,"Тебе достался вариант №: ".$message[1]." из ".$count_gifs,$replyMarkup);
+        	 }
+}
+
 
         if(in_array($user_id_group, $vip_users)){
         	if (preg_match_all("/(?<![\w\d])(tits [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
