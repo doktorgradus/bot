68bac3b9156314de34b04bf68020632e06b97448
systemfailure <kefzce@gmail.com>
2016-11-08 03:01:24 +0200

    new butts

--- core/answers.php
+++ core/answers.php
@@ -67,7 +67,7 @@ switch($message) {
             include 'core/commands/tits/tits.php';
             include 'core/commands/gif/gif.php';
             include 'core/commands/buts/buts_id.php';
-                        sendMessage($chat_id,"Общая статистика: \n Сисек в базе: <b>{$count_tits}</b> \n Жоп в базе: <b>{$count_butts}</b> \n Гифок в базе: <b>{$count_gifs}</b> \n Вип юзеров: <b>{$count_vips}</b> \n @phphelperbot - дата обновления [".date('H:i:s')."]",$msgid);
+                        sendMessage($chat_id,"Общая статистика: \n Сисек в базе: <b>{$count_tits}</b> \n Задниц в базе: <b>{$count_butts}</b> \n Гифок в базе: <b>{$count_gifs}</b> \n Вип юзеров: <b>{$count_vips}</b> \n @phphelperbot - дата обновления [".date('H:i:s')."]",$msgid);
         break;
         case '/bash':
             include 'commands/bash.php';
@@ -93,7 +93,7 @@ switch($message) {
             include 'core/commands/tits/tits.php';
             include 'core/commands/gif/gif.php';
             include 'core/commands/buts/buts_id.php';
-            sendMessage($chat_id,"Общая статистика: \n Сисек в базе: <b>{$count_tits}</b> \n Жоп в базе: <b>{$count_butts}</b> \n Гифок в базе: <b>{$count_gifs}</b> \n Вип юзеров: <b>{$count_vips}</b> \n @phphelperbot - дата обновления [".date('H:i:s')."]",$msgid);
+            sendMessage($chat_id,"Общая статистика: \n Сисек в базе: <b>{$count_tits}</b> \n Задниц в базе: <b>{$count_butts}</b> \n Гифок в базе: <b>{$count_gifs}</b> \n Вип юзеров: <b>{$count_vips}</b> \n @phphelperbot - дата обновления [".date('H:i:s')."]",$msgid);
         break;
         case '/bash@phphelperbot':
             include 'commands/bash.php';
