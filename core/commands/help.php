984a72b02b7251ba7aa0bf94653826d3185e34b4
systemfailure <kefzce@gmail.com>
2016-11-08 02:08:32 +0200

    updated

--- core/commands/help.php
+++ core/commands/help.php
@@ -1,7 +1,8 @@
 <?php
 // Отправляем приветственный текст.
-    $answer = array(); // Инициализируем пустой массив
-    $answer[] = "Простенький автоответчик и отличный помощник: \n Список комманд: - [/commands] \n Бот <b>OpenSource</b> и постоянно обновляется, присылайте свои Pull Request <a href='https://github.com/kefzce/phptelegrambot'>GitHub</a> \n Author - @oneerror "; 
-    $rand = mt_rand(0, count($answer) - 1);
-    sendMessage($chat_id,$answer[$rand],$msgid);
+    $help = [
+    "Бот понимает следующие команды: \n <b>/tits</b> - отправляет сиськи. \n <b>/butts</b> - отправляет попу. \n <b>/gif</b> - отправляет гифку. \n Так же можно делать выборки по значению: \n <b>/gif 5</b> - покажет гифку под № <b>5</5> \n <b>/tits 150</b> - покажет сиськи под № <b>150</b> \n <b>/butts 150</b> - покажет задницу под № <b>150</b> \n Посмотреть статистику - /stats",
+    ];
+    $rand = array_rand($help);
+    sendMessage($chat_id,$help[$rand],$msgid);
 ?>
