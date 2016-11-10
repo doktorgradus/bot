360680fcba8a27b5a380d496947195f0daf92801
systemfailure <kefzce@gmail.com>
2016-11-10 03:38:43 +0200

    updated /web/ see test.php

--- core/answers.php
+++ core/answers.php
@@ -3,13 +3,19 @@ echo "answers loadedd   <br>";
 switch($message) {
 	case '/goth':  
      if(in_array($user_id_group, $vip_users2)){
-        	if (preg_match_all("/(?<![\w\d])(goth [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
-	include 'core/commands/goth/goth.php';
-    include 'core/commands/goth/preloader.php';
-    sendPhoto($chat_id,$goth_id[$message[1]],$msgid,"Тебе достался вариант №: ".$message[1]." из ".$count_goth,$replyMarkup);
+     if (preg_match_all("/(?<![\w\d])(goth [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
+			$message = explode(" ", $message);
+			include 'core/commands/goth/goth.php';
+    		$inline_button1 = array("text"=>"👍","callback_data" =>'/voteup');
+		    $inline_button2 = array("text"=>"👎","callback_data" =>'/votedown');
+		    $inline_keyboard = [[$inline_button1,$inline_button2]];
+		    $keyboard=array("inline_keyboard"=>$inline_keyboard);
+		    $replyMarkup = json_encode($keyboard);
+		    sendChatAction($chat_id, "upload_photo");
+		    sendPhoto($chat_id,$goth_id[$message[1]],$msgid,"Тебе достался вариант №: ".$message[1]." из ".$count_goth,$replyMarkup);
         	 }
 }else{
-	include 'core/commands/permission_denied.php';
+	include 'commands/permission_denied.php';
 }
         break;
         case '/tits':  
