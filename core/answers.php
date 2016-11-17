017a4ae9892bfddb3c237d95cadfed1f4910e611
systemfailure <kefzce@gmail.com>
2016-11-17 09:57:34 +0200

    tovar list

--- core/answers.php
+++ core/answers.php
@@ -42,13 +42,14 @@ switch($message) {
     $inline_keyboard = [[$inline_button1,$inline_button2]];
     $keyboard=array("inline_keyboard"=>$inline_keyboard);
     $replyMarkup = json_encode($keyboard); 
-     sendMessage($chat_id,"case test msg",$msgid,$replyMarkup);
+     sendMessage2($chat_id, "ok", $replyMarkup);
     break;
 }
 switch($data){
     case '/plz':
-    sendMessage($chat_id_in, "plz");
+    sendMessage2($chat_id_in, "plz");
     break;
 }
-
-?>
+function sendMessage2($chat_id, $message, $replyMarkup) {
+  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&reply_markup=' . $replyMarkup);
+}
