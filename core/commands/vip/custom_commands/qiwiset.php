9276d0daf94575c2e395d10ddc39e173c2ade98c
systemfailure <kefzce@gmail.com>
2016-11-17 08:37:56 +0200

    updated QIWI Branch

--- core/commands/vip/custom_commands/qiwiset.php
+++ core/commands/vip/custom_commands/qiwiset.php
@@ -4,8 +4,8 @@ if(in_array($user_id_group, $vip_users)){
             if (preg_match_all("/(?<![\w\d])(qiwiset [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
     //sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
     $message = explode(" ", $message);
-    $inline_button1 = array("text"=>"Сохранить","callback_data" =>'Сохранить');
-    $inline_button2 = array("text"=>"Изменить","callback_data" =>'Изменить');
+    $inline_button1 = array("text"=>"Сохранить","callback_data" =>'accept_qiwi');
+    $inline_button2 = array("text"=>"Изменить","callback_data" =>'change_qiwi');
     $inline_keyboard = [[$inline_button1,$inline_button2]];
     $keyboard=array("inline_keyboard"=>$inline_keyboard);
     $replyMarkup = json_encode($keyboard);
