778d0a42f87b170e9bd36ec4c684b417be15aa3d
systemfailure <kefzce@gmail.com>
2016-11-17 14:43:56 +0200

    test

--- core/commands/vip/kick_user.php
+++ core/commands/vip/kick_user.php
@@ -3,7 +3,7 @@ if(in_array($user_id_group, $vip_users)){
         	if (preg_match_all("/(?<![\w\d])(kick [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
     	//sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
 	$message = explode(" ", $message);
-	//sendMessage($chat_id,"Кикаем юзера {$message[1]}",$msgid);
+	sendMessage($chat_id,"Кикаем юзера {$message[1]}",$msgid);
 	sendMessage($chat_id,"Записали в базу юзера <b>{$message[1]}</b>  дату [".date('H:i:s')."] причину <b>{$message[2]}</b> \n Посмотреть - <a href='http://telegrambotv2.herokuapp.com/banlist.txt'>банлист</a>");
 	$fp = fopen("banlist.txt", "a"); // Открываем файл в режиме записи 
 	$mytext = "[".date('H:i:s')."]"."admin: {$user_first_name_group} "." user_id: {$message[1]} reason: {$message[2]} \r\n"; // Исходная строка
--- core/commands/vip/loader.php
+++ core/commands/vip/loader.php
@@ -1,11 +1,11 @@
 <?php
  require 'core/commands/vip/kick_user.php';
- require 'core/commands/vip/catch_sticker_from_non_vip.php';
+ // require 'core/commands/vip/catch_sticker_from_non_vip.php';
  require 'core/commands/vip/getChatAdministrators.php';
- require 'core/commands/vip/custom_commands/custom_goth.php';
- require 'core/commands/vip/custom_commands/custom_gifs.php';
- require 'core/commands/vip/custom_commands/custom_tits.php';
- require 'core/commands/vip/custom_commands/custom_butts.php'; 
+ // require 'core/commands/vip/custom_commands/custom_goth.php';
+ // require 'core/commands/vip/custom_commands/custom_gifs.php';
+ // require 'core/commands/vip/custom_commands/custom_tits.php';
+ // require 'core/commands/vip/custom_commands/custom_butts.php'; 
  require 'core/commands/vip/custom_commands/qiwiset.php'; 
 echo "VIP FUNCTION & CUSTOM_COMMANDS LOADED";
 ?>
