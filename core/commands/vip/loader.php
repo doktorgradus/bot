b710a8d4f0120ce2126b42534c5c696f35d50308
systemfailure <kefzce@gmail.com>
2016-11-17 15:52:59 +0200

    asd

--- core/commands/vip/loader.php
+++ core/commands/vip/loader.php
@@ -1,7 +1,7 @@
 <?php
-require 'core/commands/vip/ban_user.php';
-require 'core/commands/vip/kick_user.php';
-require 'core/commands/vip/getChatAdministrators.php';
+// require 'core/commands/vip/ban_user.php';
+// require 'core/commands/vip/kick_user.php';
+// require 'core/commands/vip/getChatAdministrators.php';
 require 'core/commands/vip/custom_commands/qiwiset.php';  
 // echo "VIP FUNCTION & CUSTOM_COMMANDS LOADED";
 ?>
