15088d16d8616e7235803f7a63e20862b73647dd
systemfailure <kefzce@gmail.com>
2016-11-17 09:38:58 +0200

    tovar list

--- core/answers.php
+++ core/answers.php
@@ -8,15 +8,15 @@ switch($message) {
         $inline_keyboard = [[$inline_button1,$inline_button2]];
         $keyboard=array("inline_keyboard"=>$inline_keyboard);
         $replyMarkup = json_encode($keyboard);
-        sendMessage($chat_id,"Ув.пользователь у вас не установлен Qiwi кошелек, для проведения транказций в системе, пожалуйста укажите следующим сообщением /qiwiset его, иначе вы не сможете пользоватся ботом.",$msgid,$replyMarkup);
+        sendMessage($chat_id,"Ув.пользователь у вас не установлен Qiwi кошелек, для проведения транказций в системе, пожалуйста укажите следующим сообщением /qiwiset его, иначе вы не сможете пользоватся ботом. \n Если у вас нет кошелька вызовите команду /qiwihowto",$msgid,$replyMarkup);
     break;
-	case '/test':  
-    $inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
-    $inline_button2 = array("text"=>"work plz","callback_data"=>'/rules');
+	case '/qiwihowto':  
+    $inline_button1 = array("text"=>"Зарегистрировать кошелек QIWI","url"=>"https://qiwi.com/");
+    $inline_button2 = array("text"=>"Видеотуториал","url"=>"https://www.youtube.com/watch?v=-u5ZIR9YjTk");
     $inline_keyboard = [[$inline_button1,$inline_button2]];
     $keyboard=array("inline_keyboard"=>$inline_keyboard);
     $replyMarkup = json_encode($keyboard); 
-     sendMessage($chat_id,"ok",$msgid,$replyMarkup);
+     sendMessage($chat_id,"Вы выбрали помощью по настройке Qiwi кошелька",$msgid,$replyMarkup);
     break;
   default:
             //include 'commands/default/default.php';
