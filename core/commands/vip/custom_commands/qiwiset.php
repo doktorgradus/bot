b0b9e3691facb485afd31cc9e8239b6002197a06
systemfailure <kefzce@gmail.com>
2016-11-17 08:22:06 +0200

    upd

--- core/commands/vip/custom_commands/qiwiset.php
+++ core/commands/vip/custom_commands/qiwiset.php
@@ -1,16 +1,17 @@
 <?php
-echo "QIWI SET LOAD";
-//if(in_array($user_id_group, $vip_users)){
-        	if (preg_match_all("/(?<![\w\d])(/qiwiset [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
-    	sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
-	$message = explode(" ", $message);
-    $inline_button1 = array("text"=>"👍","callback_data" =>'Сохранить кошелек');
-    $inline_button2 = array("text"=>"👎","callback_data" =>'Изменить');
+echo "QIWI SET LOAD <br>";
+if(in_array($user_id_group, $vip_users)){
+            if (preg_match_all("/(?<![\w\d])(qiwiset [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
+    sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
+    $message = explode(" ", $message);
+    $inline_button1 = array("text"=>"👍","callback_data" =>'/voteup');
+    $inline_button2 = array("text"=>"👎","callback_data" =>'/votedown');
     $inline_keyboard = [[$inline_button1,$inline_button2]];
     $keyboard=array("inline_keyboard"=>$inline_keyboard);
     $replyMarkup = json_encode($keyboard);
-    sendMessage($chat_id,"Ты успешно установил свой кошелек {$message[1]}",$msgid,$replyMarkup);
-        	 }
-//}
+    //sendPhoto($chat_id,$photo_id[$message[1]],$msgid,"Тебе достался вариант №: ".$message[1]." из ".$count_tits,$replyMarkup);
+    sendMessage($chat_id,"Твой киви кошелек ".$message[1],$msgid,$replyMarkup);
+             }
+}
 
 ?>
