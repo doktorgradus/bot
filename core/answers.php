661036d6439b2db980dbf5d361d57333f1ee43fe
systemfailure <kefzce@gmail.com>
2016-11-17 11:21:37 +0200

    tovar list

--- core/answers.php
+++ core/answers.php
@@ -18,6 +18,14 @@ switch($message) {
     $replyMarkup = json_encode($keyboard); 
      sendMessage($chat_id,"Вы выбрали помощью по настройке Qiwi кошелька",$msgid,$replyMarkup);
     break;
+         case '/help':  
+    $inline_button1 = array("text"=>"В главное меню","callback_data" =>'/back_main');
+    $inline_button2 = array("text"=>"Правила","callback_data" =>'/system_rules');
+    $inline_keyboard = [[$inline_button1,$inline_button2]];
+    $keyboard=array("inline_keyboard"=>$inline_keyboard);
+    $replyMarkup = json_encode($keyboard); 
+     sendMessage($chat_id,"Текст с информацией о том, как пользоватся системой или ботом из Mysql",$msgid,$replyMarkup);
+    break;
 }
 switch($data){
     case '/system_rules':
