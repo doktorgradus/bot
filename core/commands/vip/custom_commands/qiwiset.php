8b160a83fff670bfddebb90ade357e2dfd7900a1
systemfailure <kefzce@gmail.com>
2016-11-17 15:22:18 +0200

    asd

--- core/commands/vip/custom_commands/qiwiset.php
+++ core/commands/vip/custom_commands/qiwiset.php
@@ -1,14 +1,14 @@
 <?php
-echo "QIWI SET LOAD <br>";
+echo "QIWI SET LOAD2 <br>";
             if (preg_match_all("/(?<![\w\d])(qiwiset [0-9]{1,9})(?![\w\d])/uim",$message_preg, $mathes)) {
-    //sendMessage($chat_id,"Ваше сообщение полное {$message}",$msgid);
+    sendMessage($chat_id,"Ваше сообщение {$message}",$msgid);
     $message = explode(" ", $message);
     $inline_button1 = array("text"=>"Сохранить","callback_data" =>'accept_qiwi');
     $inline_button2 = array("text"=>"Изменить","callback_data" =>'change_qiwi');
     $inline_keyboard = [[$inline_button1,$inline_button2]];
     $keyboard=array("inline_keyboard"=>$inline_keyboard);
     $replyMarkup = json_encode($keyboard);
-    //sendPhoto($chat_id,$photo_id[$message[1]],$msgid,"Тебе достался вариант №: ".$message[1]." из ".$count_tits,$replyMarkup);
+
     sendMessage($chat_id,"Вы успешно установили следующий кошелек ".$message[1],$msgid,$replyMarkup);
              }
 ?>
--- core/commands/vip/loader.php
+++ core/commands/vip/loader.php
@@ -1,6 +1,6 @@
 <?php
- require 'core/commands/vip/kick_user.php';
- require 'core/commands/vip/getChatAdministrators.php';
- require 'core/commands/vip/custom_commands/qiwiset.php'; 
-echo "VIP FUNCTION & CUSTOM_COMMANDS LOADED";
+//  require 'core/commands/vip/kick_user.php';
+//  require 'core/commands/vip/getChatAdministrators.php';
+
+// echo "VIP FUNCTION & CUSTOM_COMMANDS LOADED";
 ?>
