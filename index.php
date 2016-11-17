94bcfb4b4fb4fbb8ea5ae116b0a6d4c974d2d79f
systemfailure <kefzce@gmail.com>
2016-11-17 15:37:40 +0200

    ads

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 //require __DIR__ . '/vendor/autoload.php'; //
 require 'core/functions.php';               //
 require 'core/settings.php';                //
-require 'core/answers.php';  
- require 'core/commands/vip/custom_commands/qiwiset.php';                //
+require 'core/answers.php';  				//
+require 'core/commands/vip/loader.php';		            //
  //require 'core/catch.php';                //
-// require 'core/commands/vip/loader.php';     //
+// require 'core/commands/vip/loader.php'   //
 //////////////////////////////////////////////
 echo "<hr><b> All files loaded correctly, SemsGood !!! </b></hr><br>";
 echo '<img src="https://static-cdn.jtvnw.net/emoticons/v1/64138/3.0">';
