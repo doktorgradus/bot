845a884ce3687d4809c421925cd8b8ee2aef8ec5
systemfailure <kefzce@gmail.com>
2016-11-18 13:10:16 +0200

    asd

--- core/answers.php
+++ core/answers.php
@@ -2,7 +2,7 @@
 echo "answers loadedd   <br>";
 switch($message) {
   case '/banlist':
-  sendMessage($chat_id,$ban_users,$msgid,$replyMarkup);
+  sendMessage($chat_id,"Список забаненых хуесосов {$ban_users}",$msgid,$replyMarkup);
   break;
 
 	case '/goth':  
