ffe28d0cfe0a6b691f4c6f58da6bef3825e78cd2
systemfailure <kefzce@gmail.com>
2016-11-18 10:56:14 +0200

    asd

--- libs/db.php
+++ libs/db.php
@@ -1,3 +1,5 @@
 <?php
-R::setup( 'mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'].,$_ENV['DB_LOGIN'], $_ENV['DB_PASSWORD']);
+require 'libs/rb.php';
+echo "DB inluaded";
+R::setup( 'mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'].$_ENV['DB_LOGIN'], $_ENV['DB_PASSWORD']);
 ?>
