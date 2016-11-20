ea14478468bf9eff201a3bd6a509742ceceb3abc
systemfailure <kefzce@gmail.com>
2016-11-20 14:57:42 +0200

    initial qiwi first commit

--- core/commands/gif/preloader.php
+++
@@ -1,8 +0,0 @@
-<?php
-$inline_button1 = array("text"=>"👍","callback_data"=>'/voteup');
-    $inline_button2 = array("text"=>"👎","callback_data"=>'/votedown');
-    $inline_keyboard = [[$inline_button1,$inline_button2]];
-    $keyboard=array("inline_keyboard"=>$inline_keyboard);
-    $replyMarkup = json_encode($keyboard);
-    sendChatAction($chat_id, "upload_document");
-?>
--- core/commands/leave/leave.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    sendMessage($chat_id,"Как скажешь..",$msgid);
-    LeaveChat($chat_id);
-
-?>
