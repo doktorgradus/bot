fc1c0f487e37f9151abbb6951389a90e45b78d47
systemfailure <kefzce@gmail.com>
2016-11-22 12:54:53 +0200

    asd

--- index.php
+++ index.php
@@ -14,27 +14,6 @@ require 'core/commands/vip/loader.php';     //
 // 	//kickChatMember($chat_id,$user_id);
 // }
 // }
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
-
-$link = mysqli_connect($server,$username,$password) or die ('Error connecting to mysql: ' . mysqli_error($link).'\r\n');
-
-if (!($result=mysqli_query($link,$sql))) {
-        printf("Error: %s\n", mysqli_error($link));
-    }
-
-while( $row = mysqli_fetch_row( $result ) ){
-        if (($row[0]!="information_schema") && ($row[0]!="mysql")) {
-            echo $row[0]."\r\n";
-        }
-    }
 echo "<hr><b> All files loaded correctly, SemsGood !!! </b></hr><br>";
 echo '<img src="https://static-cdn.jtvnw.net/emoticons/v1/64138/3.0">';
 echo '<a href="/web">Web form</a>';
