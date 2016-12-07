d86131286dd93247809c22f770d8d36dab198d44
doktorgradus <doktorgradus@gmail.com>
2016-12-07 15:48:21 +0300

    Update db.php

--- libs/db.php
+++ libs/db.php
@@ -1,6 +1,5 @@
 <?php
 require 'libs/rb.php';
 echo "DB inluaded <br>";
-    R::setup( 'mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_8d1979a47f79417',
-        'b578e4cd9c58c1', '6b62dfab' ); //for both mysql or mariaDB
+    R::setup( '', '', '' ); //for both mysql or mariaDB
 ?>
